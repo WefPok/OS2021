b44bea0815b8b5269576b7a9c11d0e755b2d1bf5
WefPok <46328702+WefPok@users.noreply.github.com>
2021-10-27 00:01:32 +0300

    Delete week10 directory

--- week10/ex2/ex2.sh
+++
@@ -1,7 +0,0 @@
-mkdir week01
-mkdir week10
-echo "Nikita" >> week01/file.txt
-link week01/file.txt week10/_ex2.txt
-ls -i week01/file.txt
-find ./ -inum 5324330 >> ex2.txt
-find week10/ -inum 5324330 -exec rm {} \;  
